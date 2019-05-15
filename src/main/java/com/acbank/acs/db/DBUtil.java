ffed9f0822883f54d224ca470ef5334a1784629c
Robin R <robinr@gmail.com>
2019-05-15 23:25:47 +0530

    code standart in DBUtil

--- src/main/java/com/acbank/acs/db/DBUtil.java
+++ src/main/java/com/acbank/acs/db/DBUtil.java
@@ -12,22 +12,22 @@ public class DBUtil {
 	static String mysqlUrl = "jdbc:mysql://192.168.40.124:3306/acs";
 	static String mysqlUser = "root";
 	static String mysqlPassword = "root";
-	
+
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName(mySqlDriverClass);
 		Connection connection = DriverManager.getConnection(mysqlUrl, mysqlUser, mysqlPassword);
 		return connection;
 	}
-	
+
 	public static int update(PreparedStatement preparedStatement) throws SQLException {
 		int result = 0;
 		result = preparedStatement.executeUpdate();
 		return result;
 	}
-	
+
 	public static ResultSet fetch(PreparedStatement preparedStatement) throws SQLException {
 		ResultSet result = preparedStatement.executeQuery();
 		return result;
 	}
-	
+
 }
